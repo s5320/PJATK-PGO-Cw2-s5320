3b0e5b763cbc2e449b71d111a2eeac5d96ae044a
s5320 <s5320@pjwstk.edu.pl>
2026-04-05 19:33:33 +0200

    Update class Czytelnik.java - Zad3 początek tworzenie klasy Biblioteka.java

--- src/minibliblioteka/Biblioteka.java
+++ src/minibliblioteka/Biblioteka.java
@@ -1,4 +1,15 @@
 package minibliblioteka;
 
 public class Biblioteka {
+    private Ksiazka[] ksiazki;
+    private int liczbaKsiazek;
+
+    public Biblioteka(int pojemnosc) {
+        this.ksiazki = new Ksiazka[pojemnosc];
+    }
+
+    public void dodajKsiazke(Ksiazka ksiazka) {
+        // logika dodawania do tablicy
+    }
+
 }
