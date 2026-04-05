30ca10e64f4d074175af2dac9a811e2fe7c84bd8
s5320 <s5320@pjwstk.edu.pl>
2026-04-05 14:56:56 +0200

    Update class Ksiazka, tests on Main

--- src/minibliblioteka/Ksiazka.java
+++ src/minibliblioteka/Ksiazka.java
@@ -6,5 +6,70 @@ public class Ksiazka {
     private int liczbaStron;
     private boolean dostepna;
 
+    public String getAutor() {
+        return autor;
+    }
+
+    private void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getTytul() {
+        return tytul;
+    }
+
+    private void setTytul(String tytul) {
+        this.tytul = tytul;
+    }
+
+    public int getLiczbaStron() {
+        return liczbaStron;
+    }
+
+    private void setLiczbaStron(int liczbaStron) {
+        this.liczbaStron = liczbaStron;
+    }
+
+    public boolean isDostepna() {
+        return dostepna;
+    }
+
+    private void setDostepna(boolean dostepna) {
+        this.dostepna = dostepna;
+    }
+
+    public Ksiazka(String autor, String tytul, int liczbaStron, boolean dostepna) {
+        if (autor.isEmpty()){
+            throw new IllegalArgumentException("Autor nie może być pusty");
+        }else this.autor = autor;
+        if (tytul.isEmpty()){
+            throw new IllegalArgumentException("Tytuł nie może być pusty");
+        }else this.tytul = tytul;
+        if (liczbaStron <= 0) {
+            System.out.println();
+            throw new IllegalArgumentException("Książka musi zawierać strony");
+        }else this.liczbaStron = liczbaStron;
+        this.dostepna = dostepna;
+    }
+
+    void getInfo() {
+        System.out.println("Autor: " + autor);
+        System.out.println("Tytul: " + tytul);
+        System.out.println("LiczbaStron: " + liczbaStron);
+    }
+    void borrowBook(){
+        if (isDostepna()){
+            System.out.println("Wypozyczam książkę:");
+            getInfo();
+        } else System.out.println("Książka niedostępna");
+
+        setDostepna(false);
+    }
+    void returnBook(){
+        if (isDostepna()){
+            System.out.println("Nie można zwrócić dostępnej książki");
+        }else System.out.println("Książka "+tytul+" została zwrócona");
+        setDostepna(true);
+    }
 
 }
--- src/minibliblioteka/Main.java
+++ src/minibliblioteka/Main.java
@@ -2,6 +2,14 @@ package minibliblioteka;
 
 public class Main {
     public static void main(String[] args) {
+        Ksiazka ks1 = new Ksiazka("Ja","Wariacje",1000,true);
+        Ksiazka ks2 = new Ksiazka("On","Obligacje w domu",500,true);
+        Ksiazka ks3 = new Ksiazka("Ona","Kwitnące magnolie",700,true);
+        Ksiazka ks4 = new Ksiazka("My","Ogień",100,true);
+        ks1.borrowBook();
+        ks1.borrowBook();
+        ks1.returnBook();
+        ks1.borrowBook();
 
     }
 }
